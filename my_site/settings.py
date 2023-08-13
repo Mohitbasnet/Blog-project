5827d5aecfb511cbb2a1e2e4b55db2be552f3c01
mohitbasnet08 <118841933+Mohitbasnet@users.noreply.github.com>
2023-08-13 16:27:06 +0545

    Update settings.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -130,9 +130,11 @@ USE_TZ = True
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # MEDIIA_ROOT = BASE_DIR / "uploads"
 # MEDIA_URL = "/files/"
+STATIC_URL = 'static/'
+STATICFILES_DIRS =os.path.join(BASE_DIR,'satic'),
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles_build','static')
 
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
 MEDIA_URL = '/files/'
 STATICFILES_DIRS = (
